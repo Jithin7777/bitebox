5507cb8371aff548186c8d51f327a6ed5fa5e930
Jithin7777 <133039735+Jithin7777@users.noreply.github.com>
2026-05-26 22:04:58 +0530

    update url

--- FoodDeliveryApp_frontend/src/assets/adminAssets.js
+++ FoodDeliveryApp_frontend/src/assets/adminAssets.js
@@ -5,6 +5,8 @@ import profile_image from './admin_profile_image.png'
 import upload_area from './admin_upload_area.png'
 import parcel_icon from './admin_parcel_icon.png'
 
+
+export const url="https://bitebox-backend-w9cw.onrender.com"
 export const adminAssets ={
     logo,
     add_icon,
